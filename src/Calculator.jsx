4ab32ad276e91930c80b4ef7047c0758f65e36d2
mr-ajaaj <mohamedridaajaaj25@gmail.com>
2026-01-30 00:01:22 +0100

    feat: implement operator selection logic

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,14 +4,18 @@ export default function Calculator() {
   const [previous, setPrevious] = useState("");
   const [operator, setOperator] = useState(null);
 
-    const appendNumber = (num) => {
+  const appendNumber = (num) => {
     if (num === "." && current.includes(".")) return;
     setCurrent(current + num);
   };
 
+  const chooseOperator = (op) => {
+    if (current === "") return;
+    if (previous !== "") Calculator();
+    setOperator(op);
+    setPrevious(current);
+    setCurrent("");
+  };
 
-
-  return(
-          <div></div>
-  )  
+  return <div></div>;
 }
